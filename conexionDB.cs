02be58824a0907d6cfc38c358f37b9f3dff1b1d5
ikkot <ikkot>
2019-09-29 17:40:26 -0300

    Conexion DB

---
+++ conexionDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace tst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync()
+        {
+            var document = new BsonDocument
+            {
+            {"firstname", BsonValue.Create("Peter")},
+            {"lastname", new BsonString("Mbanugo")},
+            { "subjects", new BsonArray(new[] {"English", "Mathematics", "Physics"}) },
+            { "class", "JSS 3" },
+            { "age", 45}
+            };
+
+            var client = new MongoClient("mongodb+srv://ikkot:ikkot1234@cluster0-cw0gi.mongodb.net/test?retryWrites=true&w=majority");
+            var database = client.GetDatabase("test");
+            var tes = database.GetCollection<BsonDocument>("students");
+            await tes.InsertOneAsync(document);
+        }
+
+    }
+}
